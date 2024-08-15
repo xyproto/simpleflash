5810ffc0c4cda635d374d1b784707565d5630619
Alexander F. Rødseth <rodseth@gmail.com>
2024-08-15 13:02:20 +0200

    Initial commit

---
+++ gemini.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/vertexai/genai"
+	"github.com/allegro/bigcache/v3"
+	"github.com/xyproto/env"
+	"github.com/xyproto/multimodal"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+)
+
+func init() {
+	go func() {
+		InitTranslationCache()
+	}()
+}
+
+const (
+	projectLocation = env.Str("PROJECT_LOCATION", "europe-west4")
+	projectID       = env.Str("PROJECT_ID", "44444444444")
+	temperature     = 0.0
+	textModelName   = "gemini-1.5-flash-001"
+)
+
+var (
+	timeout = 3 * time.Minute
+	verbose = env.Bool("VERBOSE")
+
+	queryCache *bigcache.BigCache = nil
+
+	// Create credentials using Google's default credentials
+	genaiClient = func() *genai.Client {
+		ctx := context.Background()
+		creds, err := google.FindDefaultCredentials(ctx, "https://www.googleapis.com/auth/cloud-platform")
+		if err != nil {
+			log.Fatalf("Failed to obtain default credentials: %v", err)
+		}
+		// Create a genai.Client using the credentials
+		genaiClient, err := genai.NewClient(ctx, projectID, location, option.WithCredentials(creds))
+		if err != nil {
+			log.Fatalf("failed to create genai client: %v", err)
+		}
+		return genaiClient
+	}()
+)
+
+func SetTimeout(timeout time.Duration) {
+	timeout = timeout
+}
+
+// InitTranslationCache initializes the BigCache cache
+func InitTranslationCache() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config := bigcache.DefaultConfig(24 * time.Hour)
+	config.HardMaxCacheSize = 256 // MB
+	config.StatsEnabled = false
+	config.Verbose = false
+	c, err := bigcache.New(ctx, config)
+	if err != nil {
+		return err
+	}
+	queryCache = c
+	return nil
+}
+
+// QueryGemini processes a prompt with optional temperature, base64-encoded data, and MIME type for the data.
+// The temperature, base64data, and dataMimeType are optional. The function is generic to handle any type of data.
+func QueryGemini(prompt string, temperature *float64, base64Data, dataMimeType, customModelName *string) (string, error) {
+	modelName := textModelName
+	if customModelName != nil {
+		modelName = *customModelName
+	}
+
+	// Generate a unique cache key based on prompt and optionally on temperature, base64Data, and dataMimeType
+	cacheKeyComponents := prompt
+	if temperature != nil {
+		cacheKeyComponents += fmt.Sprintf("%f", *temperature)
+	}
+	if base64Data != nil {
+		cacheKeyComponents += *base64Data
+		modelName = multiModalModelName
+		if customModelName != nil {
+			modelName = *customModelName
+		}
+	}
+	if dataMimeType != nil {
+		cacheKeyComponents += *dataMimeType
+	}
+	if customModelName != nil {
+		cacheKeyComponents += *customModelName
+	}
+	cacheKey := fmt.Sprintf("%x", sha256.Sum256([]byte(cacheKeyComponents)))
+
+	// Check cache for existing entry
+	if queryCache != nil {
+		if entry, err := queryCache.Get(cacheKey); err == nil {
+			return string(entry), nil
+		}
+	}
+
+	// Initialize the multimodal instance, using the provided temperature if available
+	mm := multimodal.New(modelName, 0.0) // Default temperature
+	mm.SetTimeout(timeout)
+	if temperature != nil {
+		mm = multimodal.New(modelName, float32(*temperature))
+	}
+	mm.AddText(prompt)
+
+	// If base64Data and dataMimeType are provided, decode the data and add it to the multimodal instance
+	if base64Data != nil && dataMimeType != nil {
+		data, err := base64.StdEncoding.DecodeString(*base64Data)
+		if err != nil {
+			return "", fmt.Errorf("failed to decode base64 data: %v", err)
+		}
+		mm.AddData(*dataMimeType, data)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	// Submit the multimodal query and process the result
+	result, err := mm.SubmitToClient(ctx, genaiClient)
+	if err != nil {
+		return "", fmt.Errorf("failed to process response: %v", err)
+	}
+
+	result = strings.TrimSpace(result)
+
+	// Store the new result in the cache
+	if queryCache != nil {
+		_ = queryCache.Set(cacheKey, []byte(result))
+	}
+
+	return result, nil
+}
+
+// CountTextTokens tries to count the number of tokens in the given prompt, using the VertexAI API
+func CountTextTokens(prompt string, customModelName *string) (int, error) {
+	modelName := textModelName
+	if customModelName != nil {
+		modelName = *customModelName
+	}
+	mm := multimodal.New(modelName, 0.0)
+	mm.SetTimeout(timeout)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	return mm.CountTextTokensWithClient(ctx, genaiClient, prompt)
+}
---
+++ go.mod
@@ -0,0 +1,46 @@
+module github.com/xyproto/simpleflash
+
+go 1.22.6
+
+require (
+	cloud.google.com/go/vertexai v0.12.0
+	github.com/allegro/bigcache/v3 v3.1.0
+	github.com/xyproto/env v1.9.1
+	github.com/xyproto/multimodal v1.3.1
+	golang.org/x/oauth2 v0.22.0
+	google.golang.org/api v0.192.0
+)
+
+require (
+	cloud.google.com/go v0.115.0 // indirect
+	cloud.google.com/go/aiplatform v1.68.0 // indirect
+	cloud.google.com/go/auth v0.8.1 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.3 // indirect
+	cloud.google.com/go/compute/metadata v0.5.0 // indirect
+	cloud.google.com/go/iam v1.1.12 // indirect
+	cloud.google.com/go/longrunning v0.5.11 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/s2a-go v0.1.8 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.52.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0 // indirect
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20240730163845-b1a4ccb954bf // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240725223205-93522f1f2a9f // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240730163845-b1a4ccb954bf // indirect
+	google.golang.org/grpc v1.64.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
